ee0927778f782b410385f154ebc38950e8d6981a
student_32 <student_32@ool-ad02060a.dyn.optonline.net>
2018-09-13 10:50:05 -0400

    changed server.js

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -1,5 +1,20 @@
-module.exports = {
-  host: process.env.DB_HOST || 'localhost',
-  port: process.env.DB_PORT || 5432,
-  database: process.env.DB_NAME || 'wdipedia',
+// module.exports = {
+//   host: process.env.DB_HOST || 'localhost',
+//   port: process.env.DB_PORT || 5432,
+//   database: process.env.DB_NAME || 'wdipedia',
+// };
+
+/**
+ * @module dbConfig
+ * @desc this file contains all the connection strings
+ * to connect to the database server
+ */
+
+// Our database is either at some URL,
+// or configured at some host:port
+// TODO: [1] Export your database config
+module.exports = process.env.DATABASE_URL || {
+  host:     'localhost',
+  port:     5432,
+  database: 'wdipedia',
 };
